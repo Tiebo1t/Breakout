b0689e0732726a9eddb8375e14fdac3691ca526e
Thomas <2201871@uad.ac.uk>
2025-10-17 12:19:32 +0100

    Fixed compiler errors

--- Breakout/PowerupFireBall.h
+++ Breakout/PowerupFireBall.h
@@ -6,7 +6,7 @@ public:
     PowerupFireBall(sf::RenderWindow* window, Paddle* paddle, Ball* ball);
     ~PowerupFireBall();
 
-    std::pair<POWERUPS, float> applyEffect() override; // Method to apply the power-up effect
-
+    std::pair<POWERUPS, float> applyEffect(); // Method to apply the power-up effect
+    // override
 };
 
